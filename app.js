317bc3afc532a8e839d916e46a640bc0e83b65c0
Amaresh Sahoo <eb.amareshsahoo@gmail.com>
2023-11-29 22:52:22 +0530

    2nd commit

---
+++ app.js
@@ -0,0 +1,43 @@
+// DOM Manipulation
+
+
+
+
+
+// getElementById()
+
+
+const title = document.getElementById('main-heading');
+
+console.log(title)
+
+
+
+// getElementByClassName()
+
+
+// const listItem = document.getElementsByClassName('list-items')
+// console.log(listItem)
+
+
+
+// getElementByTagName()
+
+
+const listItem = document.getElementsByTagName('li')
+console.log(listItem)
+
+
+
+// querySelector()
+
+
+// const container = document.querySelector('div');
+// console.log(container);
+
+
+// querySelectorAll()
+
+
+const container = document.querySelectorAll('div');
+console.log(container);
